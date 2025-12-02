caa6e0413369e1c84593eb86320bd3cf491ef71f
lily047 <173460097+lily047@users.noreply.github.com>
2025-12-02 20:16:51 +0530

    Update gender.py

--- gender.py
+++ gender.py
@@ -10,32 +10,35 @@ import psycopg2
 import sys 
 import os 
 
-conn = psycopg2.connect(database = sys.argv[1], 
-                        user = os.environ.get('PGUSER'), 
-                        password = os.environ.get('PGPASSWORD'), 
-                        host = os.environ.get("PGHOST"), 
-                        port = os.environ.get("PGPORT"))
-
-cursor = conn.cursor()
-
 with open('name.txt', 'r') as f:
     student_name = f.readline()
+try:
+  conn = psycopg2.connect(database = sys.argv[1], 
+                          user = os.environ.get('PGUSER'), 
+                          password = os.environ.get('PGPASSWORD'), 
+                          host = os.environ.get("PGHOST"), 
+                          port = os.environ.get("PGPORT"))
+  
+  cursor = conn.cursor()
+  
+  
+  query = '''SELECT s.gender
+  FROM students s 
+  WHERE student_fname = %s
+  '''
+  cursor.execute(query, (student_name,))
+  
+  result = cursor.fetchone()
+  
+  
+  if result[0] == 'M':
+      print('MALE')
+  else:
+      print('FEMALE')
 
+except(Exception, psycopg2.DatabaseError) as error:
+  print(error)
 
-
-query = '''SELECT s.gender
-FROM students s 
-WHERE student_fname = %s
-'''
-cursor.execute(query, (student_name,))
-
-result = cursor.fetchone()
-
-
-if result[0] == 'M':
-    print('MALE')
-else:
-    print('FEMALE')
-
-if conn is not None:
-    conn.close()
+finally:
+  if conn is not None:
+      conn.close()
